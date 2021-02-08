714defcf7fd84b9eb150356dab7e77ef5c145979
alorodlla <alorodlla@alum.us.es>
2021-02-08 13:16:02 +0100

    Modificados archivos para Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,7 +165,7 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
-BASEURL= "https://egc-app-examen.herokuapp.com/"
+BASEURL= "https://egc-app-examen.herokuapp.com"
 
 APIS = {}
 
